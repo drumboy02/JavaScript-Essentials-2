bc6f3cab79f92354ee0e13badcdf848b2ec201ea
drumboy02 <153551032+drumboy02@users.noreply.github.com>
2025-11-12 16:27:41 -0500

    commented out sample solution

--- module-2/code-challenge-1.js
+++ module-2/code-challenge-1.js
@@ -98,56 +98,56 @@ let sendEmail = (from, to, message) => {
 
 
 // sample solution:
-function sendEmail(from, to, message) {}
+// function sendEmail(from, to, message) {}
 
-class User {
-    constructor({name, surname, email, role}) {
-        this.name = name;
-        this.surname = surname;
-        this.email = email;
-        this.role = role;
-        this.messages = [];
-        this.courses = [];
-    }
+// class User {
+//     constructor({name, surname, email, role}) {
+//         this.name = name;
+//         this.surname = surname;
+//         this.email = email;
+//         this.role = role;
+//         this.messages = [];
+//         this.courses = [];
+//     }
 
-    addCourse(course, level) {
-        for(let i=0; i < this.courses.length; i++) {
-            if(this.courses[i].course === course) {
-                return;
-            }
-        }
-        this.courses.push({course, level});
-    }
+//     addCourse(course, level) {
+//         for(let i=0; i < this.courses.length; i++) {
+//             if(this.courses[i].course === course) {
+//                 return;
+//             }
+//         }
+//         this.courses.push({course, level});
+//     }
 
-    removeCourse(course) {
-        for(let i=0; i < this.courses.length; i++) {
-            if(this.courses[i].course === course) {
-                this.courses.splice(i,1);
-                break;
-            }
-        }
-    }
+//     removeCourse(course) {
+//         for(let i=0; i < this.courses.length; i++) {
+//             if(this.courses[i].course === course) {
+//                 this.courses.splice(i,1);
+//                 break;
+//             }
+//         }
+//     }
 
-    editCourse(course, level) {
-        for(let i=0; i < this.courses.length; i++) {
-            if(this.courses[i].course === course) {
-                this.courses[i].level = level;
-                break;
-            }
-        }
-    }
+//     editCourse(course, level) {
+//         for(let i=0; i < this.courses.length; i++) {
+//             if(this.courses[i].course === course) {
+//                 this.courses[i].level = level;
+//                 break;
+//             }
+//         }
+//     }
 
-    sendMessage(from, message) {
-        this.messages.push({from: from.email, to: this.email, content: message});
-        sendEmail(from.email, this.email, message);
-    }
+//     sendMessage(from, message) {
+//         this.messages.push({from: from.email, to: this.email, content: message});
+//         sendEmail(from.email, this.email, message);
+//     }
 
-    showMessagesHistory() {
-        for(let message of this.messages) {
-            console.log(`${message.from} -> ${message.to}: ${message.content}`)
-        }
-    }
-};
+//     showMessagesHistory() {
+//         for(let message of this.messages) {
+//             console.log(`${message.from} -> ${message.to}: ${message.content}`)
+//         }
+//     }
+// };
 
 let student1 = new User({name: 'Rafael', surname: 'Fife', email: 'rfife@rhyta.com', role: 'student'});
 let student2 = new User({name: 'Kelly', surname: 'Estes', email: 'k_estes@dayrep.com', role: 'student'});
